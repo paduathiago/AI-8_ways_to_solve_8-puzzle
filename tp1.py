60b6aea5fb02bf36a12ee37b6ded6cb8e6549ea3
Thiago Padua <paduathiago@outlook.com>
2023-10-06 16:31:36 -0300

    Implemented uniform cost search. Explored is now a list and takes node.states as elements.

--- tp1.py
+++ tp1.py
@@ -1,6 +1,8 @@
 import sys
 import numpy as np
 import queue
+import heapq
+
 
 class Node:
     def __init__(self, matrix, parent, depth, cost):
@@ -9,42 +11,51 @@ class Node:
         self.depth = depth
         self.cost = cost
 
+    def __lt__(self, other):
+        return self.cost < other.cost
+
     def generate_childen_nodes(self):
         children_nodes = []
         for i in range(len(self.state)):
             for j in range(len(self.state[i])):
                 if self.state[i][j] == 0:
                     if i > 0:
-                        new_node = Node(np.copy(self.state), self, self.depth + 1, self.cost + 1)
+                        new_node = Node(np.copy(self.state),
+                                        self, self.depth + 1, self.cost + 1)
                         children_nodes.append(new_node)
                         children_nodes[-1].state[i][j] = children_nodes[-1].state[i-1][j]
                         children_nodes[-1].state[i-1][j] = 0
                     if i < len(self.state) - 1:
-                        new_node = Node(np.copy(self.state), self, self.depth + 1, self.cost + 1)
+                        new_node = Node(np.copy(self.state),
+                                        self, self.depth + 1, self.cost + 1)
                         children_nodes.append(new_node)
                         children_nodes[-1].state[i][j] = children_nodes[-1].state[i+1][j]
                         children_nodes[-1].state[i+1][j] = 0
                     if j > 0:
-                        new_node = Node(np.copy(self.state), self, self.depth + 1, self.cost + 1)
+                        new_node = Node(np.copy(self.state),
+                                        self, self.depth + 1, self.cost + 1)
                         children_nodes.append(new_node)
                         children_nodes[-1].state[i][j] = children_nodes[-1].state[i][j-1]
                         children_nodes[-1].state[i][j-1] = 0
                     if j < len(self.state[i]) - 1:
-                        new_node = Node(np.copy(self.state), self, self.depth + 1, self.cost + 1)
+                        new_node = Node(np.copy(self.state),
+                                        self, self.depth + 1, self.cost + 1)
                         children_nodes.append(new_node)
                         children_nodes[-1].state[i][j] = children_nodes[-1].state[i][j+1]
                         children_nodes[-1].state[i][j+1] = 0
                     return children_nodes
 
+
 def read_input():
     alg = sys.argv[1]
-    puzzle = np.array(sys.argv[2:11]).reshape(3,3).astype(int)
+    puzzle = np.array(sys.argv[2:11]).reshape(3, 3).astype(int)
     is_there_print = sys.argv[11] if len(sys.argv) == 12 else False
     if is_there_print != 'PRINT':
         print("invalid command line arguments")
         return None
     else:
         return alg, puzzle, is_there_print
+
 
 def process_input(alg, matrix, is_there_print):
     root_node = Node(matrix, None, 0, 0)
@@ -60,7 +71,11 @@ def process_input(alg, matrix, is_there_print):
     elif alg == 'I':
         pass
     elif alg == 'U':
-        pass
+        result = uniform_cost_search(root_node)
+        print_result(result, is_there_print)
+    else:
+        print("invalid algorithm")
+
 
 def print_state(matrix):
     for i in range(len(matrix)):
@@ -71,35 +86,66 @@ def print_state(matrix):
                 print(matrix[i][j], end=' ')
         print()
 
+
 def is_goal(node):
-    goal = np.array([[1,2,3],[4,5,6],[7,8,0]])
+    goal = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
     return np.array_equal(node.state, goal)
+
 
 def bfs(root_node):
     if is_goal(root_node):
         return root_node
-    
+
     frontier = queue.Queue()
-    explored = set()
+    explored = []
     frontier.put(root_node)
 
     while not frontier.empty():
         current_node = frontier.get()
-        explored.add(current_node)
+        explored.append(current_node.state)
         children_nodes = current_node.generate_childen_nodes()
         for child in children_nodes:
-            if child not in explored and child not in frontier.queue:
+            if not any(np.all(np.equal(child.state, element)) for element in explored) and \
+            not any(np.all(np.equal(child.state, node.state)) for node in frontier.queue):
                 if is_goal(child):
                     return child
                 frontier.put(child)
-    
+
     return None
+
+
+def uniform_cost_search(root_node):
+    frontier = [root_node]
+    heapq.heapify(frontier)
+    explored = []
+
+    while len(frontier) > 0:
+        current_node = heapq.heappop(frontier)
+        if is_goal(current_node):
+            return current_node
+        
+        explored.append(current_node.state)
+        children_nodes = current_node.generate_childen_nodes()
+        
+        for child in children_nodes:
+            if not any(np.all(np.equal(child.state, element)) for element in explored) and \
+            not any(np.all(np.equal(child.state, node.state)) for node in frontier):
+                heapq.heappush(frontier, child)
+            elif any(np.all(np.equal(child.state, node.state)) for node in frontier):
+                for node in frontier:
+                    if np.all(np.equal(child.state, node.state)):
+                        if child.cost < node.cost:
+                            frontier.remove(node)
+                            heapq.heappush(frontier, child)
+
+    return None
+
 
 def print_result(result_node, is_there_print=False):
     operations = queue.LifoQueue()
     total_cost = result_node.cost
     current_node = result_node
-    
+
     if is_there_print:
         while current_node != None:
             operations.put(current_node)
@@ -113,5 +159,6 @@ def print_result(result_node, is_there_print=False):
 
 def main():
     process_input(*read_input())
+
 
 main()
