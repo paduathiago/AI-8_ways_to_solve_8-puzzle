f8452688e7acb8103f0a54891469d9f9fc405b1b
paduathiago <paduathiago@outlook.com>
2023-10-05 23:38:48 -0300

    func to generate children nodes

--- tp1.py
+++ tp1.py
@@ -3,9 +3,9 @@ import sys
 
 def read_input():
     alg = sys.argv[1]
-    matrix = np.array(sys.argv[2:11]).reshape(3,3).astype(int)
+    puzzle = np.array(sys.argv[2:11]).reshape(3,3).astype(int)
     is_there_print = sys.argv[11] if len(sys.argv) == 12 else False
-    return alg, matrix, is_there_print
+    return alg, puzzle, is_there_print
 
 def print_state(matrix):
     for i in range(len(matrix)):
@@ -15,6 +15,29 @@ def print_state(matrix):
             else:
                 print(matrix[i][j], end=' ')
         print()
+
+def generate_childen_nodes(matrix):
+    children_nodes = []
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 0:
+                if i > 0:
+                    children_nodes.append(np.copy(matrix))
+                    children_nodes[-1][i][j] = children_nodes[-1][i-1][j]
+                    children_nodes[-1][i-1][j] = 0
+                if i < len(matrix) - 1:
+                    children_nodes.append(np.copy(matrix))
+                    children_nodes[-1][i][j] = children_nodes[-1][i+1][j]
+                    children_nodes[-1][i+1][j] = 0
+                if j > 0:
+                    children_nodes.append(np.copy(matrix))
+                    children_nodes[-1][i][j] = children_nodes[-1][i][j-1]
+                    children_nodes[-1][i][j-1] = 0
+                if j < len(matrix[i]) - 1:
+                    children_nodes.append(np.copy(matrix))
+                    children_nodes[-1][i][j] = children_nodes[-1][i][j+1]
+                    children_nodes[-1][i][j+1] = 0
+                return children_nodes
 
 """
 def process_input(alg, matrix, is_there_print):
@@ -30,4 +53,5 @@ def process_input(alg, matrix, is_there_print):
 
 read_input_A = read_input()
 print_state(read_input_A[1])
+print(generate_childen_nodes(read_input_A[1]))
 
