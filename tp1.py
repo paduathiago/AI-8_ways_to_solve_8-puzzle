5c74350fa3204165b28751c8096cafb044c17a3e
paduathiago <paduathiago@outlook.com>
2023-10-05 23:13:08 -0300

    adding function to print each board state

--- tp1.py
+++ tp1.py
@@ -4,10 +4,30 @@ import sys
 def read_input():
     alg = sys.argv[1]
     matrix = np.array(sys.argv[2:11]).reshape(3,3).astype(int)
-    is_there_print = True if len(sys.argv) == 12 else False
+    is_there_print = sys.argv[11] if len(sys.argv) == 12 else False
     return alg, matrix, is_there_print
 
+def print_state(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 0:
+                print(' ', end=' ')
+            else:
+                print(matrix[i][j], end=' ')
+        print()
+
+"""
+def process_input(alg, matrix, is_there_print):
+    if alg == 'A':
+        return alg_A(matrix, is_there_print)
+    elif alg == 'B':
+    
+    elif alg == 'G':
+    elif alg == 'H':
+    elif alg == 'I':
+    elif alg == 'U':
+"""
+
 read_input_A = read_input()
-for i in range(0,3):
-    print(read_input_A[i])
+print_state(read_input_A[1])
 
